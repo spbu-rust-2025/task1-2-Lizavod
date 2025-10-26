dd764565fe0f65fc25b04ca06a5991d27d587f2d
Елизавета Водолазская <elizavetavxa@gmail.com>
2025-10-26 23:38:02 +0300

    added calculation of the sum of numbers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
+use std::io;
 
+fn main() {
+	
+	let mut sum_n: i32 = 0;
+	loop{
+		let mut input = String::new();
+		io::stdin()
+			.read_line(&mut input)
+			.expect("Couldn't read the line");
+		let number = input.trim();	
+		if number == "-1"{
+			break;
+		}
+		match number.parse::<i32>() {
+			Ok(num) => {
+				if num > 0{
+					sum_n += num;
+				}
+				else{
+					println!("NaN");
+					return;
+				}
+			}
+			Err(_) => {
+				println!("NaN");
+				return;
+			}
+		}
+	}
+	println!("{}", sum_n);
 }
